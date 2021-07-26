c58e7e35a8e4de8cbbe4856da8c3f883ac834e8d
Pedro Harzer <pedroharzer@gmail.com>
2021-07-26 17:12:29 -0300

    V1.1 Parametrização

--- scraping.py
+++ scraping.py
@@ -8,47 +8,32 @@ import time
 driver = webdriver.Firefox()
 driver.get("https://cointelegraph.com/tags/markets")
 titles = []
-def getHtml():
-    html = driver.page_source
-    driver.quit()
-    return html
-
-def newGetHtml():
+def readMoreNews(btnClass):
     scrollNumber = 10
     for i in range(scrollNumber):
         try:
             print(i)
             time.sleep(3)
             element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'posts-listing__more-btn'))
+                EC.presence_of_element_located((By.CLASS_NAME, btnClass))
             )
             element.click()
         except:
             print('a mimir')
             time.sleep(3)
-            newGetHtml()
+            readMoreNews()
         finally:
             if i == scrollNumber - 1:
                 return driver.page_source
-def privacyPolicy():
-    try:
-        cookies = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'privacy-policy__accept-btn'))
-        )
-        cookies.click()
-    finally:
-        try:
-            driver.find_element_by_class_name("privacy-policy__text")
-        except:
-            try:
-                cookies = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "container privacy-policy__wrp"))
-        )
-            except:
-                return True
-def soupMess():
-    privacyPolicy()
-    soup = BeautifulSoup(newGetHtml(), 'html.parser')
+def privacyPolicy(privacyBtnClass):
+    cookies = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, privacyBtnClass))
+    )
+    cookies.click()
+def soupMess(privacyPolicyCheck = True):
+    if privacyPolicyCheck:
+        privacyPolicy('privacy-policy__accept-btn')
+    soup = BeautifulSoup(readMoreNews('posts-listing__more-btn'), 'html.parser')
     sopa = soup.find_all('span', 'post-card-inline__title')
     for itens in sopa:
         titles.append(itens.get_text())
