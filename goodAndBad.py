17a64904eb5ece1fc03c2858a4037ad26c9a4e9e
Pedro Harzer <pedroharzer@gmail.com>
2021-07-27 14:43:09 -0300

    Gathering data and converting it to good or bad signs

---
+++ goodAndBad.py
@@ -0,0 +1,5 @@
+goodWords = ['consolidate', 'higher low', 'rise', 'rising',' rises', 'record-breaker', 'gains','rallies',' bullish',
+'gain'] #need more data
+badWords = ['crash', 'crash', 'drop', 'bottomed', 'resistance', 'weakness','falling', 'falls', 'low', 'fear'] #need more data
+def define(article): #define if an article is bumping or dumping a coin
+    pass
